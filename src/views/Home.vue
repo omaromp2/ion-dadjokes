7c9cefe5a49011df503b3b98e6eafd3403b8ba25
omar <omargponce@gmail.com>
2021-06-30 11:25:08 -0400

    some changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,6 +30,10 @@
           <ion-icon name="chat" ></ion-icon> Tell me a Joke
         </ion-button> -->
 
+      <ion-refresher slot="fixed" @ionRefresh="recall($event)">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+
         <div v-if="jokes.length == 0">
           No Jokes at the moment. Please pull my finger
         </div>
@@ -39,18 +43,25 @@
 
           <ion-card v-for="joke in jokes" :key="joke" >
             <ion-card-header>
-              <ion-card-title>DadJokes</ion-card-title>
+              <ion-card-title>
+                <ion-icon name="glasses" size="large" ></ion-icon>
+              </ion-card-title>
               <ion-card-subtitle>This is your joke</ion-card-subtitle>
             </ion-card-header>
 
             <ion-card-content>
               {{joke.joke}}
+
             </ion-card-content>
+
+            <ion-button @click="shareJoke(joke.joke)" >Share <ion-icon name="share" ></ion-icon> </ion-button>
+
           </ion-card>
 
         </div>
 
       </div>
+
     </ion-content>
     <ion-fab vertical="top" horizontal="end" slot="fixed">
           <ion-fab-button>
@@ -61,16 +72,20 @@
             <ion-fab-button @click="clear()" ><ion-icon name="trash"></ion-icon></ion-fab-button>
           </ion-fab-list>
         </ion-fab>
+
+
   </ion-page>
 </template>
 
 <script >
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonFab, 
-  IonFabButton, IonFabList } from '@ionic/vue';
+  IonFabButton, IonFabList, IonRefresher, IonRefresherContent } from '@ionic/vue';
 import { addIcons } from 'ionicons';
-import { trashBinOutline, chatbubbleOutline, addCircleOutline } from 'ionicons/icons';
+import { trashBinOutline, chatbubbleOutline, addCircleOutline, glassesOutline, shareSocialOutline } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 import axios from 'axios';
+import { Plugins } from "@capacitor/core";
+const { Share } = Plugins;
 
 export default defineComponent({
   name: 'Home',
@@ -87,14 +102,18 @@ export default defineComponent({
     IonCardTitle, 
     IonFab, 
     IonFabButton, 
-    IonFabList
+    IonFabList, 
+    IonRefresher,
+    IonRefresherContent,
   }, 
 
   created(){
     addIcons({
       'trash': trashBinOutline,
       'chat': chatbubbleOutline,
-      'add' : addCircleOutline
+      'add' : addCircleOutline,
+      'glasses': glassesOutline,
+      'share': shareSocialOutline,
     });
     this.getJoke();
   },
@@ -107,7 +126,6 @@ export default defineComponent({
   }, 
   methods:{
     getJoke(){
-
       // const header = {'accept': 'application/json'}; 
 
       axios.get('https://icanhazdadjoke.com/',{
@@ -134,7 +152,31 @@ export default defineComponent({
     clear(){
       // this.joke = '';
       this.jokes = []; 
+    }, 
+    
+    recall(event){
+      console.log("Hago refresh: ", event);
+
+      // llamo la funcion para empujar el chiste 
+      this.getJoke(); 
+
+      setTimeout(()=> {
+        console.log("acabe");
+        event.target.complete();
+      },2000);
+
+    }, 
+
+    async shareJoke(joke) {
+      await Share.share({
+        title: 'Share Joke', 
+        text: joke, 
+        dialogTitle: "Share with buddies",
+      });
     }
+
+
+
   }
 
 });
